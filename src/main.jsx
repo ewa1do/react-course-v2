d9d7e5c0a57c96fee7a077faf9815870bd3a56cd
Eduardo Vera <this.eduardovera@gmail.com>
2024-10-17 09:52:35 -0400

    thinking in react: components, composition and reusability

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import StarRating from "./StarRating.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color={"red"}
+      className="test"
+      defaultRating={3}
+    />
+  </StrictMode>
+);
